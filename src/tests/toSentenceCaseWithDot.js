506ea6a1998adabc05a73c2d488992cee8dc30fc
oprogramador <uomodislesia@gmail.com>
2019-12-06 08:33:50 +0100

    no space after dot case

--- src/tests/toSentenceCaseWithDot.js
+++ src/tests/toSentenceCaseWithDot.js
@@ -17,4 +17,12 @@ describe('toSentenceCaseWithDot', () => {
 
     expect(result).to.equal('Lorem ipsum dolor sit amet. Consectetur adipiscing elit. Sed do eiusmod tempor incididunt.');
   });
+
+  it('recognizes a new sentence only if there is a space after dot', () => {
+    const input = 'loREm IPsum DOLOR sit ameT. consectetur Adipiscing elit. sed DO (in the U.S.A and the E.U.) eiusmod tempor incididunt.';
+
+    const result = toSentenceCaseWithDot(input);
+
+    expect(result).to.equal('Lorem ipsum dolor sit amet. Consectetur adipiscing elit. Sed do (in the u.s.a and the e.u.) eiusmod tempor incididunt.');
+  });
 });
